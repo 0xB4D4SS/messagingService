95c0a149c8326b3b886f730bda80352efb032088
AstereY <0xb4d4ss@gmail.com>
2023-04-19 22:33:45 +0400

    init

---
+++ app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+
+// Transports expose the service to the network. In this first example we utilize JSON over HTTP.
+func main() {
+	svc := stringService{}
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
